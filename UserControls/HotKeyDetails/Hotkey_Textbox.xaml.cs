76b6f046b75efec5b37ec68acde815161536e3fa
projectSBC <project.sbc@outlook.com>
2023-02-22 07:08:34 -0500

    update

--- UserControls/HotKeyDetails/Hotkey_Textbox.xaml.cs
+++ UserControls/HotKeyDetails/Hotkey_Textbox.xaml.cs
@@ -4,12 +4,14 @@ using Handheld_Control_Panel.Classes.Global_Variables;
 using Handheld_Control_Panel.Classes.UserControl_Management;
 using Handheld_Control_Panel.Styles;
 using MahApps.Metro.Controls;
+using SharpDX.XInput;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,6 +23,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Handheld_Control_Panel.UserControls
 {
@@ -32,12 +35,21 @@ namespace Handheld_Control_Panel.UserControls
         private string windowpage = "";
         private string usercontrol = "";
 
+
+        //var for hotkey input
+        private DispatcherTimer gamepadTimer = new DispatcherTimer(DispatcherPriority.Render);
+        private DispatcherTimer keyboardTimer = new DispatcherTimer();
+        private ushort controllerButtons = 0;
+        private int gamepadTimerTickCounter = 0;
+        private ushort currentGamepad = 0;
+        private ushort previousGamepad = 0;
+      
         public HotKey_Textbox()
         {
             InitializeComponent();
             //UserControl_Management.setupControl(control);
-            control.Text = Global_Variables.profiles.editingProfile.Path;
-            loadAppType();
+            control.Content = Global_Variables.hotKeys.editingHotkey.DisplayHotkey;
+         
         }
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
@@ -47,56 +59,13 @@ namespace Handheld_Control_Panel.UserControls
             
         }
 
-        private void loadAppType()
-        {
-            TextBlock textBox = (TextBlock)subText.Content;
-            switch (Global_Variables.profiles.editingProfile.AppType)
-            {
-                case "Exe":
-                    simpleIcon.Visibility = Visibility.Collapsed;
-                    control.Text = Global_Variables.profiles.editingProfile.Path;
-                    break;
-                case "Steam":
-                    materialIcon.Visibility = Visibility.Collapsed;
-                    control.Visibility= Visibility.Collapsed;
-                    button.Visibility = Visibility.Collapsed;
-                    textBox.Text = "Steam: " + Global_Variables.profiles.editingProfile.GameID;
-
-                    string imageDirectory = Properties.Settings.Default.directorySteam + "\\appcache\\librarycache\\" + Global_Variables.profiles.editingProfile.GameID + "_header";
-                    if (File.Exists(imageDirectory + ".jpg"))
-                    {
-                        imageControl.Source = new BitmapImage(new Uri(imageDirectory + ".jpg", UriKind.RelativeOrAbsolute));
-                    }
-                    else
-                    {
-                        if (File.Exists(imageDirectory + ".png"))
-                        {
-                            imageControl.Source = new BitmapImage(new Uri(imageDirectory + ".png", UriKind.RelativeOrAbsolute));
-                        }
-
-                    }
-                    break;
-                case "Epic":
-                    materialIcon.Visibility = Visibility.Collapsed;
-                    control.Visibility = Visibility.Collapsed;
-                    button.Visibility = Visibility.Collapsed;
-                    simpleIcon.Kind = MahApps.Metro.IconPacks.PackIconSimpleIconsKind.EpicGames;
-                    textBox.Text = "Epic Games: " + Global_Variables.profiles.editingProfile.GameID;
-                    break;
-
-                default:
-                    simpleIcon.Visibility = Visibility.Collapsed;
-                    break;
-
-            }
-        }
-
+       
         private void handleControllerInputs(object sender, EventArgs e)
         {
             controllerUserControlInputEventArgs args= (controllerUserControlInputEventArgs)e;
             if (args.WindowPage == windowpage && args.UserControl==usercontrol)
             {
-                if (args.Action == "Y" && control.Visibility == Visibility.Visible)
+                if (args.Action == "A" && control.Visibility == Visibility.Visible)
                 {
                     button.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
                 }
@@ -109,44 +78,169 @@ namespace Handheld_Control_Panel.UserControls
             }
         }
 
-        private void control_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if(Global_Variables.profiles.editingProfile != null )
-            {
-                Global_Variables.profiles.editingProfile.Path = control.Text;
-                if (control.Text == "")
-                {
-                    Global_Variables.profiles.editingProfile.AppType = "";
-                }
-                else
-                {
-                    Global_Variables.profiles.editingProfile.AppType = "Exe";
-                }
-            }
-           
-        }
-
-    
+      
 
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.InitialDirectory = "C:\\";
-            dialog.Filter = "Applications (.exe)|*.exe"; // Filter files by extension
-
-            // Show open file dialog box
-            bool? result = dialog.ShowDialog();
-
-            // Process open file dialog box results
-            if (result == true)
+            if (button.IsLoaded)
             {
-                control.Text = dialog.FileName;
-                Global_Variables.profiles.editingProfile.Path= control.Text;
-                Global_Variables.profiles.editingProfile.AppType= "Exe";
-           
+                if (Global_Variables.hotKeys.editingHotkey.Type == "Controller")
+                {
+                    startGamepadTimer();
+                }
+                else
+                {
+                    startKBTimer();
+                }
             }
 
         }
+
+        #region gamepad timer routines
+        private void gamepad_Tick(object sender, EventArgs e)
+        {
+            currentGamepad = ((ushort)Controller_Management.currentGamePad.Buttons);
+
+            gamepadTimerTickCounter = gamepadTimerTickCounter + 1;
+
+            //timeout after 100 ticks or just over 5 seconds
+            if (gamepadTimerTickCounter > 100)
+            {
+                stopGamepadTimer(true);
+                controllerButtons = 0;
+            }
+            //start timer to read controller for inputs
+            if (currentGamepad >= previousGamepad)
+            {
+                if (currentGamepad > 0)
+                {
+                    controllerButtons = currentGamepad;
+                }
+
+            }
+            else
+            {
+                if (currentGamepad < previousGamepad && previousGamepad != 4096)
+                {
+                    //as soon as the button combo is LESS than the previous gamepad button AND isn't the A button (value of 4096) we know they have finished pressing all the buttons and we can now figure out what the combo is
+                    stopGamepadTimer(false);
+                    //string gamepadCombo = convertControllerUshortToString(controllerButtons.ToString());
+
+                    
+                    Global_Variables.hotKeys.editingHotkey.Hotkey = controllerButtons.ToString();
+                    control.Content = Global_Variables.hotKeys.editingHotkey.DisplayHotkey;
+                }
+
+            }
+            previousGamepad = currentGamepad;
+
+
+
+
+        }
+
+        private string convertControllerUshortToString(string hotkey)
+        {
+            string gamepadCombo = "";
+            Gamepad gamepad = new Gamepad();
+
+
+            ushort uShorthotkey;
+
+            if (ushort.TryParse(hotkey, out uShorthotkey))
+            {
+                gamepad.Buttons = (GamepadButtonFlags)(uShorthotkey);
+
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.A)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "A"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.B)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "B"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.X)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "X"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.Y)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "Y"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftShoulder)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "LB"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.RightShoulder)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "RB"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftThumb)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "LStick"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.RightThumb)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "RStick"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.Start)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "Start"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.Back)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "Back"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadUp)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "DPadUp"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadDown)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "DPadDown"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadLeft)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "DPadLeft"); }
+                if (gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadRight)) { gamepadCombo = makeGamepadButtonString(gamepadCombo, "DPadRight"); }
+            }
+
+
+            return gamepadCombo;
+
+        }
+        private string makeGamepadButtonString(string currentValue, string addValue)
+        {
+            //routine to make string for 
+            if (currentValue == "")
+            {
+                return addValue;
+            }
+            else
+            {
+                return currentValue + "+" + addValue;
+            }
+
+        }
+
+
+        private void startGamepadTimer()
+        {
+            Controller_Management.suspendEventsForGamepadHotKeyProgramming = true;
+            gamepadTimer.Tick += gamepad_Tick;
+            gamepadTimer.Interval = TimeSpan.FromMilliseconds(70);
+            gamepadTimerTickCounter = 0;
+            Thread.Sleep(70);
+            gamepadTimer.Start();
+            control.Content = "...";
+        }
+        private void stopGamepadTimer(bool timedOut)
+        {
+            gamepadTimer.Stop();
+            Controller_Management.suspendEventsForGamepadHotKeyProgramming = false;
+            if (timedOut)
+            {
+                control.Content = Global_Variables.hotKeys.editingHotkey.DisplayHotkey;
+            }
+        }
+
+        private void handleKeyboardStringPress(object sender, EventArgs args)
+        {
+            control.Content = (string)sender;
+            MouseKeyHook.keyboardEvents.keyboardStringPress -= handleKeyboardStringPress;
+            keyboardTimer.Stop();
+        }
+        private void startKBTimer()
+        {
+
+            MouseKeyHook.programmingKeystroke = true;
+            keyboardTimer.Tick += keyboard_Tick;
+            keyboardTimer.Interval = new TimeSpan(0, 0, 5);
+
+            MouseKeyHook.keyboardEvents.keyboardStringPress += handleKeyboardStringPress;
+
+
+            keyboardTimer.Start();
+        }
+        private void stopKBTimer()
+        {
+            keyboardTimer.Stop();
+            MouseKeyHook.programmingKeystroke = false;
+
+        }
+
+        #endregion
+        #region keyboard timer
+        private void keyboard_Tick(object sender, EventArgs e)
+        {
+            control.Content = Global_Variables.hotKeys.editingHotkey.DisplayHotkey;
+            stopKBTimer();
+        }
+
+
+        #endregion
     }
 }
