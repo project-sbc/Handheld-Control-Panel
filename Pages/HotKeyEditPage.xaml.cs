67545b9124e3835f61d459c6ed6dc82ea2d80b5b
projectsbc <Project.SBC@outlook.com>
2023-03-13 07:06:08 -0400

    update

--- Pages/HotKeyEditPage.xaml.cs
+++ Pages/HotKeyEditPage.xaml.cs
@@ -139,6 +139,7 @@ namespace Handheld_Control_Panel.Pages
         private void Page_Unloaded(object sender, RoutedEventArgs e)
         {
             Controller_Window_Page_UserControl_Events.pageControllerInput -= handleControllerInputs;
+            Global_Variables.hotKeys.editingHotkey.LoadProfile(Global_Variables.hotKeys.editingHotkey.ID);
         }
     }
 }
