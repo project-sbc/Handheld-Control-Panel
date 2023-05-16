f0b9f91b312dc28685575f5109ebc646b413d1bc
Chris Moore <christopher.moore@philips.com>
2023-05-16 17:50:19 -0400

    added new information usercontrols

---
+++ UserControls/InformationUserControls/UXTU_Link.xaml.cs
@@ -0,0 +1,83 @@
+﻿using ControlzEx.Theming;
+using Handheld_Control_Panel.Classes;
+using Handheld_Control_Panel.Classes.Controller_Management;
+using Handheld_Control_Panel.Classes.Global_Variables;
+using Handheld_Control_Panel.Classes.TaskSchedulerWin32;
+using Handheld_Control_Panel.Classes.UserControl_Management;
+using Handheld_Control_Panel.Classes.Volume_Management;
+using Handheld_Control_Panel.Styles;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Handheld_Control_Panel.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TDP_Slider.xaml
+    /// </summary>
+    public partial class UXTU_Link : UserControl
+    {
+        private string windowpage = "";
+        private string usercontrol = "";
+        public UXTU_Link()
+        {
+            InitializeComponent();
+            setControlValue();
+          
+        }
+
+        private void setControlValue()
+        {
+            
+            
+
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Controller_Window_Page_UserControl_Events.userControlControllerInput += handleControllerInputs;
+            windowpage = WindowPageUserControl_Management.getWindowPageFromWindowToString(this);
+            usercontrol = this.ToString().Replace("Handheld_Control_Panel.Pages.UserControls.","");
+          
+        }
+              
+
+        private void handleControllerInputs(object sender, EventArgs e)
+        {
+            controllerUserControlInputEventArgs args= (controllerUserControlInputEventArgs)e;
+            if (args.WindowPage == windowpage && args.UserControl==usercontrol)
+            {
+                Classes.UserControl_Management.UserControl_Management.handleUserControl(border, control, args.Action);
+
+            }
+        }
+
+
+       
+      
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Controller_Window_Page_UserControl_Events.userControlControllerInput -= handleControllerInputs;
+          
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
+        }
+    }
+}
