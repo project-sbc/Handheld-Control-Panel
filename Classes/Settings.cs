ec79f3bf11afa503c7b2fa6bef885844c9ee86de
projectsbc <Project.SBC@outlook.com>
2023-06-03 20:23:14 -0400

    updates

---
+++ Classes/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handheld_Control_Panel.Classes
+{
+    public static class Settings
+    {
+
+
+    }
+}
