6492377e3af705f9fb932ea13464f8dd063dd442
projectsbc <Project.SBC@outlook.com>
2023-06-09 12:36:20 -0400

    save

---
+++ Classes/Profiles.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Handheld_Control_Panel.Classes
+{
+    public class Profiles: List<Profile_Main>
+    {
+
+
+
+
+
+    }
+
+
+
+    public class Profile_Main
+    {
+        //profile name is also the folder name
+        public string ProfileName { get; set; }
+        public Profile_Info profile_Info { get; set; }
+        public Profile_Exe profile_Exe { get; set; }
+        public Profile_Parameters profile_Parameters { get; set; }
+        public Profile_ControllerMapping profile_ControllerMapping { get; set; }
+
+    }
+    public class Profile_Info
+    {
+        
+        public string ProfileExe { get; set; }
+        public bool DefaultProfile
+        {
+            get { return defaultProfile; }
+            set
+            {
+                defaultProfile = value;
+                if (value == true) { VisibilityDefaultProfile = Visibility.Visible; } else { VisibilityDefaultProfile = Visibility.Collapsed; }
+            }
+        }
+        public bool defaultProfile { get; set; }
+        public bool activeProfile { get; set; }
+        public bool ActiveProfile
+        {
+            get { return activeProfile; }
+            set
+            {
+                activeProfile = value;
+                if (value == true) { VisibilityActiveProfile = Visibility.Visible; } else { VisibilityActiveProfile = Visibility.Collapsed; }
+            }
+        }
+        public Visibility VisibilityActiveProfile { get; set; } = Visibility.Collapsed;
+        public Visibility VisibilityDefaultProfile { get; set; } = Visibility.Collapsed;
+
+    }
+    public class Profile_Exe
+    {
+        public string Exe_Path { get; set; }
+        public string Exe_Type { get; set; }
+        public string Exe_ID { get; set; }
+        public string Exe_Image_Path { get; set; }
+        public int LastLaunched { get; set; } = 0;
+        public int NumberLaunches { get; set; } = 0;
+
+    }
+    public class Profile_Parameters
+    {
+        public string Offline_TDP1 { get; set; } = "";
+        public string Offline_TDP2 { get; set; } = "";
+        public string Offline_ActiveCores { get; set; } = "";
+        public string Offline_MAXCPU { get; set; } = "";
+        public string Offline_FPSLimit { get; set; } = "";
+        public string Offline_EPP { get; set; } = "";
+        public string Offline_GPUCLK { get; set; } = "";
+        public string Online_TDP1 { get; set; } = "";
+        public string Online_TDP2 { get; set; } = "";
+        public string Online_ActiveCores { get; set; } = "";
+        public string Online_MAXCPU { get; set; } = "";
+        public string Online_FPSLimit { get; set; } = "";
+        public string Online_EPP { get; set; } = "";
+        public string Online_GPUCLK { get; set; } = "";
+        public int AutoTDP_CPU_Offset { get; set; } = 0;
+        public int AutoTDP_GPU_Offset { get; set; } = 0;
+    }
+    public class Profile_ControllerMapping
+    {
+
+    }
+}
---
+++ Classes/Profiles_XML_SaveLoad.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ControlzEx.Standard;
+using Handheld_Control_Panel.Classes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Handheld_Control_Panel.Classes
+{
+    public static class Profiles_XML_SaveLoad
+    {
+        private static string hcpDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private static object lockObject = new object();
+
+        public static void Save_XML(string folderName, string objType, object objClass)
+        {
+            lock (lockObject)
+            {
+                StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "Profiles\\" + folderName + "\\" + objType  +".xml");
+                XmlSerializer xmls = null;
+                switch(objType)
+                {
+                    case "Profile_Info":
+                        xmls = new XmlSerializer(typeof(Profile_Info));
+                        Profile_Info objInfo = (Profile_Info)objClass;
+                        xmls.Serialize(sw, objInfo);
+                        objInfo = null;
+                        break;
+                    case "Profile_Exe":
+                        xmls = new XmlSerializer(typeof(Profile_Exe));
+                        Profile_Exe objExe = (Profile_Exe)objClass;
+                        xmls.Serialize(sw, objExe);
+                        objExe = null;
+                        break;
+                    case "Profile_Parameters":
+                        xmls = new XmlSerializer(typeof(Profile_Parameters));
+                        Profile_Parameters objParameters = (Profile_Parameters)objClass;
+                        xmls.Serialize(sw, objParameters);
+                        objParameters = null;
+                        break;
+                    case "Profile_ControllerMapping":
+                        xmls = new XmlSerializer(typeof(Profile_ControllerMapping));
+                        Profile_ControllerMapping objCM = (Profile_ControllerMapping)objClass;
+                        xmls.Serialize(sw, objCM);
+                        objCM = null;
+                        break;
+                }
+                sw.Dispose();
+                xmls = null;
+                
+            }
+        }
+        public static object Load_XML(string folderPath, string objType)
+        {
+            string filePath = hcpDirectory + "Profiles\\" + folderPath + ".xml";
+            object objObject = null;
+            if (File.Exists(filePath))
+            {
+                lock (lockObject)
+                {
+                    StreamReader sr = new StreamReader(filePath);
+                    XmlSerializer xmls = new XmlSerializer(typeof(Profile));
+
+                    switch (objType)
+                    {
+                        case "Profile_Info":
+                            xmls = new XmlSerializer(typeof(Profile_Info));
+                            objObject = ((Profile_Info)xmls.Deserialize(sr));
+              
+                            break;
+                        case "Profile_Exe":
+                            xmls = new XmlSerializer(typeof(Profile_Exe));
+                            objObject = ((Profile_Exe)xmls.Deserialize(sr));
+                            break;
+                        case "Profile_Parameters":
+                            xmls = new XmlSerializer(typeof(Profile_Parameters));
+                            objObject = ((Profile_Parameters)xmls.Deserialize(sr));
+                            break;
+                        case "Profile_ControllerMapping":
+                            xmls = new XmlSerializer(typeof(Profile_ControllerMapping));
+                            objObject = ((Profile_ControllerMapping)xmls.Deserialize(sr));
+                            break;
+                    }
+                    
+                    sr.Dispose();
+                    xmls = null;
+                }
+            }
+            return objObject;
+        }
+      
+
+    }
+}
